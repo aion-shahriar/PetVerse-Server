5372df2d40be7e971db3c96e3d07f9c0dc7ae6cf
aion-shahriar <aionshahriar23@gmail.com>
2026-02-13 21:12:10 +0600

    mongodb and env done

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Middleware to enable CORS
+app.use(cors());
+
+// Define the port the server will listen on
 const PORT = 3000;
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3tedund.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
 
 // Basic route: When a user visits the root URL (/)
 app.get('/', (req, res) => {
